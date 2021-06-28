d4a6ce781f45e322de67d25a0ada580590f728f2
naresh.manthrabuddi <naresh.manthrabuddi@BRILAP2530.BRILLIO.COM>
2021-06-28 13:18:31 +0530

    Second Commit

---
+++ Test Suites/Test Suite2.ts
@@ -0,0 +1,14 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<TestSuiteEntity>
+   <description></description>
+   <name>Test Suite2</name>
+   <tag></tag>
+   <isRerun>false</isRerun>
+   <mailRecipient></mailRecipient>
+   <numberOfRerun>0</numberOfRerun>
+   <pageLoadTimeout>30</pageLoadTimeout>
+   <pageLoadTimeoutDefault>true</pageLoadTimeoutDefault>
+   <rerunFailedTestCasesOnly>false</rerunFailedTestCasesOnly>
+   <rerunImmediately>false</rerunImmediately>
+   <testSuiteGuid>fc8ee0dc-6755-4cda-93e9-f87570c366cc</testSuiteGuid>
+</TestSuiteEntity>
