37adbec45f57569f7c4d45bc854aee2551e33e8f
naresh.manthrabuddi <naresh.manthrabuddi@BRILAP2530.BRILLIO.COM>
2021-06-28 20:06:49 +0530

    added the test cases

---
+++ Test Suites/TestSuite4.ts
@@ -0,0 +1,14 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<TestSuiteEntity>
+   <description>TestSuite4</description>
+   <name>TestSuite4</name>
+   <tag></tag>
+   <isRerun>false</isRerun>
+   <mailRecipient></mailRecipient>
+   <numberOfRerun>0</numberOfRerun>
+   <pageLoadTimeout>30</pageLoadTimeout>
+   <pageLoadTimeoutDefault>true</pageLoadTimeoutDefault>
+   <rerunFailedTestCasesOnly>false</rerunFailedTestCasesOnly>
+   <rerunImmediately>false</rerunImmediately>
+   <testSuiteGuid>79459eb1-96b8-42f5-968e-60481f5330b4</testSuiteGuid>
+</TestSuiteEntity>
