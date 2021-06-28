c3c7f2c8769076bcfdab2a494d73c68d3efa34e9
naresh.manthrabuddi <naresh.manthrabuddi@BRILAP2530.BRILLIO.COM>
2021-06-28 13:16:14 +0530

    First commit

---
+++ Test Suites/Test Suite1.ts
@@ -0,0 +1,44 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<TestSuiteEntity>
+   <description></description>
+   <name>Test Suite1</name>
+   <tag></tag>
+   <isRerun>false</isRerun>
+   <mailRecipient></mailRecipient>
+   <numberOfRerun>0</numberOfRerun>
+   <pageLoadTimeout>30</pageLoadTimeout>
+   <pageLoadTimeoutDefault>true</pageLoadTimeoutDefault>
+   <rerunFailedTestCasesOnly>false</rerunFailedTestCasesOnly>
+   <rerunImmediately>false</rerunImmediately>
+   <testSuiteGuid>527f3762-54f3-42fe-85be-f65afad40bae</testSuiteGuid>
+   <testCaseLink>
+      <guid>789b9881-745e-4742-ad39-6b10b6ef9c0d</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/TestCase1</testCaseId>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>d49a363a-3f5d-4fc3-99cc-47ba7c6fdde6</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/TestCase2</testCaseId>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>212e26f6-fc45-4077-b02b-89d5b32ba15e</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/TestCase3</testCaseId>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>fa292408-5967-443b-a788-e30034340887</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/TestCase4</testCaseId>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>336630ec-e265-4525-90aa-449ef4828ab3</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/TestCase5</testCaseId>
+   </testCaseLink>
+</TestSuiteEntity>
